173b9c8a7c7f81f0670157995f7e7e4b466c6d42
sainigorv <sainigorv341@gmail.com>
2022-12-06 03:55:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,26 @@ import React from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [text, setText] = React.useState(true);
+  const toggleText = () => {
+    setText(!text);
+  };
   return (
     <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
+      {text ? (
+        <div id="welcome-div">
+          <h1>Welcome to Newton School!!</h1>
+        </div>
+      ) : (
+        <div id="about-div">
+          <h1>
+            Newton School is a neo-university providing highly immersive tech
+            learning to millions of students, to tap into new-age tech
+            opportunities.
+          </h1>
+        </div>
+      )}
+
       <button id="toggle">Toggle</button>
     </div>
   );
